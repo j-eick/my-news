853b3a54b09e7d21b9676da3295f745bc3efe877
Joschka Eickhoff <jeickhoff@posteo.de>
2024-02-20 17:27:32 +0100

    added 3 more countries

--- src/utils/headlines.tsx
+++ src/utils/headlines.tsx
@@ -2,6 +2,7 @@ type HeadlinesArrayProps = {
   country: string;
   handle: string;
   url: string;
+  active: boolean;
 };
 
 export const headlinesArray: HeadlinesArrayProps[] = [
@@ -9,15 +10,36 @@ export const headlinesArray: HeadlinesArrayProps[] = [
     country: "germany",
     handle: "de",
     url: "https://newsapi.org/v2/top-headlines?country=de&apiKey=",
+    active: true,
   },
   {
     country: "usa",
     handle: "us",
     url: "https://newsapi.org/v2/top-headlines?country=us&apiKey=",
+    active: true,
   },
   {
     country: "china",
     handle: "cn",
     url: "https://newsapi.org/v2/top-headlines?country=cn&apiKey=",
+    active: true,
+  },
+  {
+    country: "france",
+    handle: "fr",
+    url: "https://newsapi.org/v2/top-headlines?country=fr&apiKey=",
+    active: false,
+  },
+  {
+    country: "russia",
+    handle: "ru",
+    url: "https://newsapi.org/v2/top-headlines?country=ru&apiKey=",
+    active: false,
+  },
+  {
+    country: "ukraine",
+    handle: "ua",
+    url: "https://newsapi.org/v2/top-headlines?country=ua&apiKey=",
+    active: false,
   },
 ];
