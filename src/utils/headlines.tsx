8bff0c94cba57608b408c8670c6bbf48497b7a85
Joschka Eickhoff <jeickhoff@posteo.de>
2024-02-21 21:06:00 +0100

    commented out 4 countries to reduce numbers of fetches

--- src/utils/headlines.tsx
+++ src/utils/headlines.tsx
@@ -18,28 +18,28 @@ export const headlinesArray: HeadlinesArrayProps[] = [
     url: "https://newsapi.org/v2/top-headlines?country=us&apiKey=",
     active: true,
   },
-  {
-    country: "china",
-    handle: "cn",
-    url: "https://newsapi.org/v2/top-headlines?country=cn&apiKey=",
-    active: true,
-  },
-  {
-    country: "france",
-    handle: "fr",
-    url: "https://newsapi.org/v2/top-headlines?country=fr&apiKey=",
-    active: false,
-  },
-  {
-    country: "russia",
-    handle: "ru",
-    url: "https://newsapi.org/v2/top-headlines?country=ru&apiKey=",
-    active: false,
-  },
-  {
-    country: "ukraine",
-    handle: "ua",
-    url: "https://newsapi.org/v2/top-headlines?country=ua&apiKey=",
-    active: false,
-  },
+  // {
+  //   country: "china",
+  //   handle: "cn",
+  //   url: "https://newsapi.org/v2/top-headlines?country=cn&apiKey=",
+  //   active: true,
+  // },
+  // {
+  //   country: "france",
+  //   handle: "fr",
+  //   url: "https://newsapi.org/v2/top-headlines?country=fr&apiKey=",
+  //   active: false,
+  // },
+  // {
+  //   country: "russia",
+  //   handle: "ru",
+  //   url: "https://newsapi.org/v2/top-headlines?country=ru&apiKey=",
+  //   active: false,
+  // },
+  // {
+  //   country: "ukraine",
+  //   handle: "ua",
+  //   url: "https://newsapi.org/v2/top-headlines?country=ua&apiKey=",
+  //   active: false,
+  // },
 ];
