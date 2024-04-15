5c8c8f5739b17097f87c3c4871b6495056950a11
Joschka Eickhoff <jeickhoff@posteo.de>
2024-04-15 17:44:05 +0200

    updateDisplayedHLs: 1st commit

---
+++ src/utils/updateDisplayedHLs.tsx
@@ -0,0 +1,54 @@
+type UncuratedHLArrayProps = {
+  country: string;
+  handle: string;
+  url: string;
+  active: boolean;
+};
+
+/**
+ *
+ * @param allHeadlines
+ * @returns true  if
+ *                1) arrays have different lengths
+ *                2) userArray !== localStorageArray
+ */
+export default function updateDisplayedHLs(
+  allHeadlines: UncuratedHLArrayProps[]
+) {
+  let needToUpdate: boolean = false;
+
+  //---get userArray
+  const userHLs = allHeadlines
+    .filter((hl) => hl.active)
+    .map((country) => country.handle);
+  console.log("userArray: " + userHLs);
+
+  //---get localStorageArray
+  let localSArray: string[] = [];
+  const dataFromLS = localStorage.getItem("localData");
+  if (dataFromLS !== null) {
+    //---get country handles from localStorage
+    const data = JSON.parse(dataFromLS);
+    localSArray = data.map((item) => item[0].handle);
+    console.log("localStorageArray: " + localSArray);
+  }
+
+  //---checks country handles
+  //---adds "true/false" to array for every checked handle
+  const truthyArray: boolean[] = [];
+  for (let i = 0; i < userHLs.length; i++) {
+    const interim = userHLs[i];
+    const check: boolean = localSArray.includes(interim);
+    truthyArray.push(check);
+  }
+  console.log(truthyArray);
+  console.log(truthyArray.includes(false));
+
+  //---userArray !== localStorageArray
+  if (truthyArray.includes(false) || userHLs.length !== localSArray.length) {
+    needToUpdate = true;
+    return needToUpdate;
+  } else {
+    return needToUpdate;
+  }
+}
