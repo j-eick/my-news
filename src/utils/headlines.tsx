48737922826eb4947fa22254e16c23b817856058
Joschka Eickhoff <jeickhoff@posteo.de>
2024-04-20 18:19:10 +0200

    headlines.tsx: Reordering

--- src/utils/headlines.tsx
+++ src/utils/headlines.tsx
@@ -9,37 +9,37 @@ export const headlinesArray: HLArrayProps[] = [
   {
     country: "Germany",
     handle: "de",
-    url: "https://newsapi.org/v2/top-headlines?country=de&apiKey=",
     active: true,
+    url: "https://newsapi.org/v2/top-headlines?country=de&apiKey=",
   },
   {
     country: "USA",
     handle: "us",
+    active: false,
     url: "https://newsapi.org/v2/top-headlines?country=us&apiKey=",
-    active: true,
   },
   {
     country: "China",
     handle: "cn",
+    active: false,
     url: "https://newsapi.org/v2/top-headlines?country=cn&apiKey=",
-    active: true,
   },
   {
     country: "France",
     handle: "fr",
-    url: "https://newsapi.org/v2/top-headlines?country=fr&apiKey=",
     active: false,
+    url: "https://newsapi.org/v2/top-headlines?country=fr&apiKey=",
   },
   {
     country: "Russia",
     handle: "ru",
-    url: "https://newsapi.org/v2/top-headlines?country=ru&apiKey=",
     active: false,
+    url: "https://newsapi.org/v2/top-headlines?country=ru&apiKey=",
   },
   {
     country: "Ukraine",
     handle: "ua",
-    url: "https://newsapi.org/v2/top-headlines?country=ua&apiKey=",
     active: false,
+    url: "https://newsapi.org/v2/top-headlines?country=ua&apiKey=",
   },
 ];
